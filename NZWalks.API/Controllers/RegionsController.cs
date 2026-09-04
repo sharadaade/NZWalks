38d240c0f0748547c656bdcc22a9ca49c53fbc92
s.aade <s.aade@DESKTOP-UGLUST6>
2026-09-04 23:38:13 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,164 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(NZWalksDbContext dbContext, IRegionRepository regionRepository,
+            IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        // GET https://localhost:portnumber/api/regions
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var regionsDomain = await regionRepository.GetAllAsync();
+
+            // Map Domain Models to DTOs
+            //var regionDto = new List<Region>();
+
+            //foreach(var regionDomain in regionsDomain)
+            //{
+            //    regionDto.Add(new Region
+            //    {
+            //        Id = regionDomain.Id,
+            //        Name = regionDomain.Name,
+            //        Code = regionDomain.Code,
+            //        RegionImageUrl = regionDomain.RegionImageUrl
+            //    });
+            //}
+
+            // Map Domain Models to DTOs
+            var regionDto = mapper.Map<List<Region>>(regionsDomain);
+
+            return Ok(regionDto);
+        }
+
+        // GET: http://localhost:portnumber/api/regions/{id}
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            // Find() works with only Primary Key
+            var regionDto = await regionRepository.GetByIdAsync(id);
+
+            // FirstOrDefault() works with other property like Id, Name, Code etc.
+            //var result = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDto == null) return NotFound();
+
+            return Ok(mapper.Map<RegionDto>(regionDto));
+        }
+
+        // POST to Create New Region
+        // POST : htts://localhost:portnumber/api/regions
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map or Convert DTO to Domain Model
+            //var regionDomainModel = new Region
+            //{
+            //    Code = addRegionRequestDto.Code,
+            //    Name = addRegionRequestDto.Name,
+            //    RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            //};
+
+            var regionDomainModel = mapper.Map<Region>(addRegionRequestDto);
+
+            // Use Domain Model to create Region
+            regionDomainModel = await regionRepository.CreateAsync(regionDomainModel);
+
+            // Map Domain Model back to DTO
+            //var regionDto = new RegionDto
+            //{
+            //    Id = regionDomainModel.Id,
+            //    Code = regionDomainModel.Code,
+            //    Name = regionDomainModel.Name,
+            //    RegionImageUrl = regionDomainModel.RegionImageUrl
+            //};
+
+            var regionDto = mapper.Map<RegionDto>(regionDomainModel);
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id}, regionDto);
+        }
+
+        //Update Region
+        // PUT : htts://localhost:portnumber/api/regions
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            //var regionDomainModel = new Region
+            //{
+            //    Code = updateRegionRequestDto.Code,
+            //    Name = updateRegionRequestDto.Name,
+            //    RegionImageUrl = updateRegionRequestDto.RegionImageUrl
+            //};
+
+            var regionDomainModel = mapper.Map<Region>(updateRegionRequestDto);
+            regionDomainModel = await regionRepository.UpdateAsync(id, regionDomainModel);
+
+            if(regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Convert Domain Model to DTO
+            //var RegionDto = new RegionDto
+            //{
+            //    Id = regionDomainModel.Id,
+            //    Code = regionDomainModel.Code,
+            //    Name = regionDomainModel.Name,
+            //    RegionImageUrl = regionDomainModel.RegionImageUrl
+            //};
+
+            var RegionDto = mapper.Map<RegionDto>(regionDomainModel);
+
+            return Ok(RegionDto);
+        }
+
+
+        // Delete Region
+        // DELETE : http://localhost:portnumber/api/regions/{id}
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var regionDomainModel = await regionRepository.DeleteAsync(id);
+                
+            if(regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //var regionDto = new RegionDto
+            //{
+            //    Id = regionDomainModel.Id,
+            //    Code = regionDomainModel.Code,
+            //    Name = regionDomainModel.Name,
+            //    RegionImageUrl = regionDomainModel.RegionImageUrl
+            //};
+
+            var regionDto = mapper.Map<RegionDto>(regionDomainModel);
+
+            return Ok(regionDto);
+        }
+            
+
+
+    }
+}
---
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -0,0 +1,10 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IWalkRepository
+    {
+        Task<List<Walk>> GetAllAsync();
+        Task<Walk> CreateAsync(Walk walk);
+    }
+}
