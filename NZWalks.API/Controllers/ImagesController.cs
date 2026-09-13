85539391e20d70c22a65798f0b0670792477b5f5
Sharad Aade <s.aade@vipspms.com>
2026-09-14 00:33:12 +0530

    image vilidate method added

--- NZWalks.API/Controllers/ImagesController.cs
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,10 +8,39 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
-
+        
         public ImagesController()
         {
             
+        }
+
+        // POST : /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+
+            if(ModelState.IsValid)
+            {
+                //User repository to Upload image
+            }
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtention = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if(!allowedExtention.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extention");
+            }
+
+            if(request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10MB, Please upload smaller size");
+            }
         }
     }
 }
